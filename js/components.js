981fbf0daa11d365a6a1b98b8dd9218157d945b4
Vasilica Cernovschi <vasilica.cernovschi@assist.ro>
2025-07-08 11:05:48 +0300

    position and name of product changed

--- js/components.js
+++ js/components.js
@@ -76,7 +76,7 @@ function showNotification(message, type = 'info', duration = 3000) {
     // Position notification
     notification.style.position = 'fixed';
     notification.style.top = '150px';
-    notification.style.right = '20px';
+    notification.style.right = '30px';
     notification.style.zIndex = '9999';
     notification.style.minWidth = '300px';
     
